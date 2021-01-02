13804e8187d3858196f4d98faf469857b84b50b3
wumanho <wumanho@tutanota.com>
2021-01-02 13:48:41 +0800

    cron test

--- exec/corn/main.go
+++ exec/corn/main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/gorhill/cronexpr"
+	"time"
 )
 
 func main() {
 	var (
 		expr *cronexpr.Expression
 		err  error
+		now time.Time
+		nextTime time.Time
 	)
-	//每分钟执行一次
-	if expr, err = cronexpr.Parse("* * * * *123"); err != nil {
+
+	if expr, err = cronexpr.Parse("*/6 * * * *"); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	expr = expr
+	//获取当前时间
+	now = time.Now()
+	//获取下次调度时间
+	nextTime = expr.Next(now)
+	fmt.Printf("当前时间:%v\n 下次调度时间:%v",now,nextTime)
 }
