7ab7cba5190b0ad24e4465e2c35d532c73989695
wumanho <wumanho@tutanota.com>
2021-05-05 22:32:12 +0800

    重新提交一下

--- exec/corn/main.go
+++ exec/corn/main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/gorhill/cronexpr"
-	"time"
-)
-
 func main() {
-	var (
-		expr *cronexpr.Expression
-		err  error
-		now time.Time
-		nextTime time.Time
-	)
-
-	if expr, err = cronexpr.Parse("*/5 * * * * * *"); err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	//获取当前时间
-	now = time.Now()
-	//获取下次调度时间
-	nextTime = expr.Next(now)
-	//设置一个定时器和回调函数
-	time.AfterFunc(nextTime.Sub(now), func() {
-		fmt.Println("被调度了，下次执行时间：",nextTime)
-	})
-
-	time.Sleep(5 * time.Second)
+	
 }
